83f8c47ca2405008fd4ded390ae528c611928f3b
Kozhukhar Dmitry <dna.coj@gmail.com>
2019-04-17 18:56:04 +0300

    10

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -5,8 +5,73 @@ import timber.log.Timber
 
 class GameViewModel : ViewModel() {
     init {
+        resetList()
+        nextWord()
         Timber.i("GameViewModel created")
     }
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+    // The current word
+    var word = ""
+
+    // The current score
+    var score = 0
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /** Methods for buttons presses **/
+
+    fun onSkip() {
+        score--
+        nextWord()
+    }
+
+    fun onCorrect() {
+        score++
+        nextWord()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            //gameFinished()
+        } else {
+            word = wordList.removeAt(0)
+        }
+
+    }
+
 
     override fun onCleared() {
         super.onCleared()
